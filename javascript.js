178ed6e5d511165ca829efba2df262517ebbc238
dora <dora.korpar@gmail.com>
2016-06-27 11:02:24 -0700

    added some more javascript

--- javascript.js
+++ javascript.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
-	var socket = io.connect('http://' + document.domain + ':' + location.port);
+	var socket = io.connect('http://localhost:8000');
+		//'http://' + document.domain + ':' + location.port);
 		socket.on('connect', function(){
-			alert("connected");
+			console.log("connected");
 		});
 });
 
 socket.emit('join', {room: room});
-
-document.getElementById('submit')
 
 // grabs inputs from textfields
 function words()
 {
 	var word1 = document.getElementById("word1");
 	var word2 = document.getElementById("word2");
-	alert("input");
+	console.log("input");
 }
 
 // generates custom id for every request
@@ -24,4 +23,13 @@ function makeid() {
 	for (var i = 0; i < 10; i++)
 		text += possible.charAt(Math.floor(Math.random() * possible.length));
 	return text;
+}
+
+function 
+
+var gen_id = makeid();
+var json = {
+	"word1": word1,
+	"word2": word2,
+	"id": gen_id
 }
